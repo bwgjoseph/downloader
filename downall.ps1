d27c054ed3df1088d9d4b6f52aae6bc5425e6d43
bwgjoseph <josephgan@live.com.sg>
2022-09-23 01:41:19 +0800

    add download all script

---
+++ downall.ps1
@@ -0,0 +1,18 @@
+cd apps
+.\apps-downloads.ps1
+cd ..
+cd github
+.\github-downloads.ps1
+cd ..
+cd plugins
+cd intellij
+.\intellij-plugin-downloads.ps1 -version 2022.2
+cd ..
+cd vscode
+.\vscode-plugin-downloads.ps1
+.\vscode-models-downloads.ps1 -username joseph -version 1.2.27
+cd ..
+cd ..
+cd winget
+.\winget-downloads.ps1
+cd ..
