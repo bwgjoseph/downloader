f214c53e629b8d0209735b85f3a8d8cffb62969d
bwgjoseph <josephgan@live.com.sg>
2025-01-08 00:24:35 +0800

    add downloader script for chrome extension

---
+++ plugins/chrome/README.md
@@ -0,0 +1,3 @@
+# Chrome Extensions
+
+To install, launch extension page, and drag `crx` file into it
---
+++ plugins/chrome/chrome-plugin-downloads.ps1
@@ -0,0 +1,21 @@
+﻿Write-Host "Starting..."
+
+$default_download_dir="./_chrome"
+
+$chromeVersion = "131.0.6778.205"
+
+# # For each application
+Get-Content .\chrome-plugins.txt | ForEach-Object {
+    $extName, $extId = $_ -split ' '
+
+    # source for url > https://stackoverflow.com/questions/72854391/how-can-i-download-a-chrome-extension-crx-file-with-the-manifest-in-version-3-gi
+    $download_url = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=$chromeVersion&acceptformat=crx2,crx3&x=id%3D$extId%26uc"
+
+    Write-Host "Starting download for $extName"
+    # -L: follow redirect
+    # --create-dirs: if not exist
+    # --silent: do not show progress
+    curl -L $download_url -o "$default_download_dir/$extName.crx" --create-dirs --silent
+}
+
+Write-Host "Completed"
