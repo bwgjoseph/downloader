23c36a50e3a898a280eedc6f71fd55b7b639a72a
bwgjoseph <josephgan@live.com.sg>
2026-05-18 00:49:54 +0800

    update intellij and fix dbeaver

--- downall.ps1
+++ downall.ps1
@@ -43,7 +43,8 @@ cd intellij
 # .\intellij-plugin-downloads.ps1 -version 2025.2.4 -build 252.27397.103
 # .\intellij-plugin-downloads.ps1 -version 2025.3.1 -build 253.29346.138
 # .\intellij-plugin-downloads.ps1 -version 2025.3.3 -build 253.31033.145
-.\intellij-plugin-downloads.ps1 -version 2026.1.1 -build 261.23567.138
+# .\intellij-plugin-downloads.ps1 -version 2026.1.1 -build 261.23567.138
+.\intellij-plugin-downloads.ps1 -version 2026.1.2 -build 261.24374.151
 cd ..
 cd vscode
 .\vscode-plugin-downloads.ps1
